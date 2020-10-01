377dd8d9ae19f4742477817d05fe494e9ff9302e
Virus <vladprodan6@gmail.com>
2020-10-01 15:19:55 +0300

    jwt

--- crmproject/crmproject/settings.py
+++ crmproject/crmproject/settings.py
@@ -27,6 +27,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated'
+    )
+}
+
 
 # Application definition
 
@@ -37,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crmmain',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
